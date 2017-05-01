59e187f332d326faed516abf1f8ec38b552f39dd
Francieli Viane <francielifrv@gmail.com>
2017-05-01 04:11:20 -0300

    Configures database

--- src/main/java/com/filemanager/FileUploadApplication.java
+++ src/main/java/com/filemanager/FileUploadApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @ComponentScan("com.filemanager")
 public class FileUploadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileUploadApplication.class, args);
+	}
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 }
