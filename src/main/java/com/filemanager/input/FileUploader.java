ee262da2c192a4d17984c86d553c96c67dd60b50
Francieli Viane <francielifrv@gmail.com>
2017-05-01 03:55:11 -0300

    Creates tmp directory when it does not exist

--- src/main/java/com/filemanager/input/FileUploader.java
+++ src/main/java/com/filemanager/input/FileUploader.java
@@ -3,7 +3,6 @@ package com.filemanager.input;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,8 +14,14 @@ public class FileUploader {
     public Path upload(MultipartFile file, String uploadToPath) throws IOException {
         byte[] bytes = file.getBytes();
 
-        Path uploadFilePath = Paths.get(uploadToPath + File.separator + file.getOriginalFilename());
+        Path directory = createTempDirectory(Paths.get(uploadToPath));
+
+        Path uploadFilePath = Paths.get(directory.toString(), file.getOriginalFilename());
 
         return Files.write(uploadFilePath, bytes);
+    }
+
+    private Path createTempDirectory(Path path) throws IOException {
+        return Files.exists(path) ? path : Files.createDirectory(path);
     }
 }
